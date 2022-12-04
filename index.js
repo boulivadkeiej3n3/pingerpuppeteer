158282256aa4828cffc0a78192b360ad0ef32246
boulivadkeiej3n3 <119795836+boulivadkeiej3n3@users.noreply.github.com>
2022-12-04 16:51:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Mongoose  = require("mongoose");
-const HTTP      = require("http");
+const Axios      = require("axios");
+const randomUserAgent = require("random-useragent");
 const ServersDB = Mongoose.model(`servers`, new Mongoose.Schema({url:String}));
 const asyncTimeout = async(duration)=>{await new Promise((resolve)=>{setTimeout(resolve,duration)}) }
 let ServersToPing = [];
@@ -10,8 +11,7 @@ connection ? console.log(`Database Connected!`): console.log(`Error Occured duri
 
 
 async function main(){
-  const Browser = await Puppeteer.launch({headless:true});
-  const ServerPage    = await Browser.newPage();
+ 
    ServersToPing = await ServersDB.find({url:{$regex:/.+/}});
   ServersToPing.slice((process.argv.slice(2))[0],(process.argv.slice(2))[1])
   //Looping forever and ping all list of servers every 2 minutes:
@@ -20,8 +20,7 @@ async function main(){
         {
            console.log(`GOTO: ${ServerURL}`);
            try{
-            (!ServerPage)?ServerPage =  await Browser.newPage():0;
-           await ServerPage.goto(ServerURL)
+              await Axios.get(ServerURL.url,{headers:{"user-agent": randomUserAgent.random()}});             
            }catch(e){if(e){ console.log(`[PINGER ERROR]: ${e.message}`)}}
            await asyncTimeout(20000);
           
